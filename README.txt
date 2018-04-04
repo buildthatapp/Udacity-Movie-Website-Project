eba49c71a06268d6f567ccac2a3ace81283869d8
Louis Magdaleno <buildthatapp@gmail.com>
2018-04-04 08:18:35 -0700

    bug fix
    * all movies now have youtube trailers that launch when the movie poster is clicked
    * code has been standardized to pep8, checked on pep8online.com
    * added comments to media.py

---
+++ README.txt
@@ -0,0 +1,9 @@
+Steps to run Movie Website
+1. Extract all contents to a folder on your machine.
+2. Open entertainment_center.py
+3. Run entertainment_center.py
+
+
+entertainment_center.py will import all required files and create a fresh_tomatoes.html file in the root directory of the project.
+
+open this html file, if not already opened, and it will display the completed Movie Website project.
--- entertainment_center.py
+++ entertainment_center.py
@@ -2,42 +2,42 @@ import fresh_tomatoes
 import media
 
 toy_story = media.Movie(
-	"Toy Story",
+    "Toy Story",
     "Toys Come to Life",
-	"toy_story.jpg",
-	"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+    "toy_story.jpg",
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie(
-	"Avatar",
-	"Life on an alien planet",
-	"avatar.jpg",
-	"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+    "Avatar",
+    "Life on an alien planet",
+    "avatar.jpg",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 avengers_infinity_war = media.Movie(
-	"Avengers Infinity War",
-	"Time is running out",
-	"infinity_war.jpg",
-	"https://www.youtube.com/watch?v=QwievZ1Tx-8&t=2s")
+    "Avengers Infinity War",
+    "Time is running out",
+    "infinity_war.jpg",
+    "https://www.youtube.com/watch?v=QwievZ1Tx-8&t=2s")
 
 school_of_rock = media.Movie(
-	"School of Rock",
-	"A new teacher is in town",
-	"school_of_rock.jpg",
-	"https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+    "School of Rock",
+    "A new teacher is in town",
+    "school_of_rock.jpg",
+    "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
 
 ratatouille = media.Movie(
-	"Ratatouille",
-	"Big dreams start small",
-	"ratatouille.jpg",
-	"https://www.youtube.com/watch?v=1yKqLNnxGZw")
+    "Ratatouille",
+    "Big dreams start small",
+    "ratatouille.jpg",
+    "https://www.youtube.com/watch?v=1yKqLNnxGZw")
 
 the_hunger_games = media.Movie(
-	"The Hunger Games",
-	"A fight for the future",
-	"hunger_games.jpg",
-	"https://www.youtube.com/watch?v=mfmrPu43DF8")
+    "The Hunger Games",
+    "A fight for the future",
+    "hunger_games.jpg",
+    "https://www.youtube.com/watch?v=mfmrPu43DF8")
 
 movies = [toy_story, avatar, avengers_infinity_war,
-	school_of_rock, ratatouille, the_hunger_games]
+          school_of_rock, ratatouille, the_hunger_games]
 
 fresh_tomatoes.open_movies_page(movies)
